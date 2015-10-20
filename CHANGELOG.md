ad3c47d8a480838e678116850bc4004fac5ff8d4
Shaurya <shauryachats@gmail.com>
2015-10-20 17:04:47 +0530

    Unary operators support extended

---
+++ CHANGELOG.md
@@ -0,0 +1,40 @@
+### Unary operators support extended (HEAD, master)
+>Tue, 20 Oct 2015 17:04:47 +0530
+
+>Author: Shaurya (shauryachats@gmail.com)
+
+>Commiter: Shaurya (shauryachats@gmail.com)
+
+
+
+
+### Paranthesis removed from operators_list (origin/master, origin/HEAD)
+>Mon, 19 Oct 2015 20:48:14 +0530
+
+>Author: Shaurya (shauryachats@gmail.com)
+
+>Commiter: Shaurya (shauryachats@gmail.com)
+
+
+
+
+### Postfix Infix Base ready
+>Mon, 19 Oct 2015 20:27:56 +0530
+
+>Author: Shaurya (shauryachats@gmail.com)
+
+>Commiter: Shaurya (shauryachats@gmail.com)
+
+
+
+
+### Initial commit
+>Mon, 19 Oct 2015 20:25:29 +0530
+
+>Author: shauryachats (shauryachats@gmail.com)
+
+>Commiter: shauryachats (shauryachats@gmail.com)
+
+
+
+
--- function.py
+++ function.py
@@ -5,15 +5,19 @@
 #	shauryachats@gmail.com
 #
 
-#The operator array is to be differentiated from other characters while splitting.
-	
-"""TODO : Add support for custom operators, like sin() or log()"""
-operators_list = ['+','-','*','/','^']
+import math
 
+#The operator array is to be differentiated from other characters while splitting.
+operators_list_binary = ['+','-','*','/','^']
+
+#The operators which take only one argument as an input - the predefined list.
+operators_list_unary = ["sin","cos","tan","cosec","sec","cot","log"]
+
+
+"""TODO : Add support for custom operators"""
 #Defines a priority order of the operators, like which operator 
 #should be applied before another, like the BODMAS rule.
 
-"""TODO : Add support for priority for custom operators"""
 priority_order = {
 	'^' : 3,
 	'*' : 2,
@@ -22,34 +26,41 @@ priority_order = {
 	'-' : 1
 }
 
-#This function splits the function string into a list
-#of lexicons, that can be processed by the infix-er.
+#Setting priority for all unary operators to be 4
 
-def string_split_into_lexicon( input_string ):
-	lexicon_list = []
+priority_order.update(dict((element,4) for element in operators_list_unary))
+
+#This function splits the function string into a list
+#of tokens, that can be processed by the infix-er.
+
+def string_split_into_token( input_string ):
+	token_list = []
 	iterating_index = 0 	
 	
-	lexicon_list.append('(')
+	token_list.append('(')
 
 	while (iterating_index < len(input_string)):
 		
 		character = input_string[iterating_index]
 
+		print character
 		#Checking if there is a number, we'll chomp it whole.
 		if (character.isdigit()):
 			temp = int(character)
 			iterating_index += 1
 			#Searching the next element : if it's a number, we take it in, if not, the loop exits.
 			
-			while (iterating_index < len(input_string) and input_string[iterating_index].isdigit()):
+			while (
+				iterating_index < len(input_string) and	input_string[iterating_index].isdigit()
+				):
 				temp = temp * 10 + int(input_string[iterating_index])
 				iterating_index += 1
 
-			lexicon_list.append(temp)
+			token_list.append(temp)
 
 		#Checking for the operators
-		elif character in operators_list or character in ['(',')']:
-			lexicon_list.append(character)
+		elif character in operators_list_binary or character in ['(',')']:
+			token_list.append(character)
 			iterating_index += 1
 
 		#Checking for plain characters
@@ -57,60 +68,69 @@ def string_split_into_lexicon( input_string ):
 			temp = character
 			iterating_index += 1
 
-			while (iterating_index < len(input_string) and input_string[iterating_index].isalpha()):
+			while (iterating_index < len(input_string) 
+				and input_string[iterating_index] not in ['(',')']
+				and input_string[iterating_index].isalpha()):
 				temp += input_string[iterating_index]
 				iterating_index += 1
 
-			lexicon_list.append(temp)
+			token_list.append(temp)
 
-	lexicon_list.append(')')
+	token_list.append(')')
 
-	return lexicon_list
+	return token_list
 
-def lexicon_list_to_postfix(lexicon_list):
+def token_list_to_postfix(token_list):
 	postfix_list = []
 	stack = []
-	for lexicon in lexicon_list:
-		if lexicon not in operators_list and lexicon not in ['(',')']:
-			postfix_list.append(lexicon)
-		else:
-			if (lexicon == '('):
-				stack.append(lexicon)
-			elif (lexicon == ')'):
-				while (stack[-1] != '('):
-					postfix_list.append(stack.pop())
-
+	for token in token_list:
+		
+		if (token == '('):
+				stack.append(token)
+		elif (token == ')'):
+			while (stack[-1] != '('):
+				postfix_list.append(stack.pop())
 				#Get rid of the '('
-				stack.pop()
+			stack.pop()
 			
-			else:
-				while stack and stack[-1] != '(' and (priority_order[stack[-1]] >= priority_order[lexicon]):
+		elif token in operators_list_binary or token in operators_list_unary:
+				while stack and stack[-1] != '(' and (priority_order[stack[-1]] >= priority_order[token]):
 					postfix_list.append(stack.pop())
 				
-				stack.append(lexicon)
+				stack.append(token)
 
-		print postfix_list
-		print stack
+		else:
+			postfix_list.append(token)
+		#print postfix_list
+		#print stack
 
+	print postfix_list
 	return postfix_list
 
-
-"""TODO : Add functionality to variables, like x and y for calculation"""
-
+"""TODO: Throw error if variable is not contained in variable_dict"""
 def postfix_to_value(postfix_list, variable_dict):
 	value = 0
 	stack = []
 	for token in postfix_list:
-		if  token not in operators_list:
-			stack.append(token)
-		else:
+		
+		#print token
+		print stack
+
+		if  token in operators_list_binary:
+			
 			value2 = stack.pop()
 			value1 = stack.pop()
 			
-			if (not isinstance(value1, int)):
+			#If value1 and value2 are not numbers, look them up in variable_dict
+			"""TODO: What happens when they are not in variable_dict??"""
+			try:
+				value1 = int(value1)
+			except ValueError:
 				value1 = variable_dict[value1]
 
-			if (not isinstance(value2, int)):
+			try:
+				value2 = int(value2)
+			except ValueError:
 				value2 = variable_dict[value2]
 
 
@@ -123,16 +143,52 @@ def postfix_to_value(postfix_list, variable_dict):
 			elif (token == '/'):
 				value = value1 / value2
 			elif (token == '^'):
-				value == value1 ** value2
+				value == math.pow(value1, value2)
+			
 			stack.append(value)
+		
+		elif token in operators_list_unary:
+
+			value = stack.pop()
+
+			try:
+				value = int(value)
+			except ValueError:
+				value = variable_dict[value]
+
+			if (token == "sin"):
+				value = math.sin(value)
+			elif (token == "cos"):
+				value = math.cos(value)
+			elif (token == "tan"):
+				value = math.tan(value)
+			elif (token == "cosec"):
+				value = 1/math.sin(value)
+			elif (token == "sec"):
+				value = 1/math.cos(value)
+			elif (token == "cot"):
+				value = 1/math.tan(value)
+			elif (token == "log"):
+				value = math.log(value)
+
+			stack.append(value)
+
+		else:
+			stack.append(token)
+			
 	return stack.pop()
 
 
 ##TEST
-x = "(5+6*x)*(3*y-4)"
+x = "cos(sin(0))"
 dicta = {
 	"x" : 2,
 	"y" : 3
 }
-print postfix_to_value(lexicon_list_to_postfix(string_split_into_lexicon(x)),dicta)
 
+y = string_split_into_token(x)
+#print y
+z = token_list_to_postfix(y)
+#print z
+aa = postfix_to_value(z,dicta)
+print aa
